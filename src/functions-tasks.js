11dee8d97371dd3e6f6e2d27aa638078c898ec4c
anthony-th <yefremov.anthony@gmail.com>
2025-04-14 19:05:25 +0300

    feat: add retry solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -137,8 +137,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let count = 0;
+    while (count < attempts) {
+      try {
+        return func();
+      } catch (err) {
+        count += 1;
+        if (count >= attempts) throw new Error(err);
+      }
+    }
+    return null;
+  };
 }
 
 /**
