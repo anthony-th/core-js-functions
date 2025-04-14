a3c1743389bcc70e5345955651791e2894deb26c
anthony-th <yefremov.anthony@gmail.com>
2025-04-14 18:28:02 +0300

    feat: add getPolynom solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -88,8 +88,14 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (!args.length) return null;
+  return (inputValue) =>
+    args.reduceRight(
+      (result, coeff, index, coefficients) =>
+        result + coeff * inputValue ** (coefficients.length - index - 1),
+      0
+    );
 }
 
 /**
